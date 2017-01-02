c2dfdb0c86e6523113a0ee629509df649075c4cc
Patrick Jominet <jompa010@gmail.com>
2017-01-02 23:26:31 +0100

    added main-page route

--- akaflieg-ui/src/app/app-routing.ts
+++ akaflieg-ui/src/app/app-routing.ts
@@ -1,10 +1,12 @@
 import {NgModule} from "@angular/core";
 import {RouterModule, Routes} from "@angular/router";
+import {MainPageComponent} from './main-page/main-page.component';
 import {UserDashboardComponent} from './dashboard/user-dashboard/user-dashboard.component';
 import {PrivacyPolicyComponent} from './privacy-policy/privacy-policy.component';
 import {TermsOfUseComponent} from './terms-of-use/terms-of-use.component';
 
 const appRoutes: Routes = [
+  {path: 'index', component: MainPageComponent},
   {path: 'dashboard', component: UserDashboardComponent},
   {path: 'privacyPolicy', component: PrivacyPolicyComponent},
   {path: 'termsOfUse', component: TermsOfUseComponent},
