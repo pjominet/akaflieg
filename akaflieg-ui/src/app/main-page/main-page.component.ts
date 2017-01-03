653c2e8ed22d67e2e4aeb10ac86aff43ad7d9840
Patrick Jominet <jompa010@gmail.com>
2017-01-03 16:55:31 +0100

    moved jquery to main-page

--- akaflieg-ui/src/app/main-page/main-page.component.ts
+++ akaflieg-ui/src/app/main-page/main-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare var $:any;
 
 @Component({
   selector: 'main-page',
@@ -9,7 +10,36 @@ export class MainPageComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit(): void {
+    /* ---- jQuery-Bootstrap Features ---- */
+    $(document).ready(function() {
+      // page scrolling feature (requires jquery.easing)
+      $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+          scrollTop: ($($anchor.attr('href')).offset().top - 50)
+        }, 1250, 'easeInOutExpo');
+        event.preventDefault();
+      });
+
+      // scroll spy (requires bootstrap.js)
+      $('body').scrollspy({
+        target: '.navbar-fixed-top',
+        offset: 51
+      });
+
+      // offset when scrolling
+      $('#mainNav').affix({
+        offset: {
+          top: 100
+        }
+      });
+
+      // hamburger menu toggle
+      $('.navbar-collapse ul li a').click(function(){
+        $('.navbar-toggle:visible').click();
+      });
+    });
   }
 
 }
