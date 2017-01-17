395ee5f6232fc03320ec89fbc94b64620c3af3da
Patrick Jominet <jompa010@gmail.com>
2017-01-17 14:11:13 +0100

    removed more Jquery Code

--- akaflieg-ui/src/app/main-page/main-page.component.ts
+++ akaflieg-ui/src/app/main-page/main-page.component.ts
@@ -1,5 +1,4 @@
-import {Component, OnInit, AfterViewInit} from '@angular/core';
-import { ScrollSpyService } from 'ng2-scrollspy';
+import {Component, OnInit} from '@angular/core';
 declare var $: any;
 
 @Component({
@@ -7,41 +6,21 @@ declare var $: any;
     templateUrl: './main-page.component.html',
     styleUrls: ['./main-page.component.scss']
 })
-export class MainPageComponent implements OnInit,AfterViewInit {
+export class MainPageComponent implements OnInit {
 
     private togglePrivacy: boolean = false;
     private toggleTerms: boolean = false;
 
-    constructor(private scrollSpyService: ScrollSpyService) {
-    }
+    constructor() {}
 
     ngOnInit(): void {
 
         /* ---- jQuery-Bootstrap Features ---- */
         $(document).ready(function () {
-            // scroll spy (requires bootstrap.js)
-            $('body').scrollspy({
-                target: '.navbar-fixed-top',
-                offset: 51
-            });
-
-            // create navbar when scrolling
-            $('#mainNav').affix({
-                offset: {
-                    top: 100
-                }
-            });
-
             // hamburger menu toggle
             $('.navbar-collapse ul li a').click(function () {
                 $('.navbar-toggle:visible').click();
             });
-        });
-    }
-
-    ngAfterViewInit() {
-        this.scrollSpyService.getObservable('window').subscribe((e: any) => {
-            console.log('ScrollSpy::window: ', e);
         });
     }
 
