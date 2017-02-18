ac77abef294d5789f6620505e55f304e8efc3b93
Patrick <jompa010@gmail.com>
2017-02-18 18:17:54 +0100

    implement basic weather api

--- akaflieg-ui/src/app/dashboard/dashboard-weather/dashboard-weather.component.ts
+++ akaflieg-ui/src/app/dashboard/dashboard-weather/dashboard-weather.component.ts
@@ -1,30 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {DashboardWeatherService} from './dashboard-weather.service'
 import {WeatherItem} from './weather-item';
 
 @Component({
-  selector: 'dashboard-weather',
-  templateUrl: './dashboard-weather.component.html',
-  styleUrls: ['./dashboard-weather.component.scss']
+    selector: 'dashboard-weather',
+    templateUrl: './dashboard-weather.component.html',
+    styleUrls: ['./dashboard-weather.component.scss']
 })
 export class DashboardWeatherComponent implements OnInit {
+    weatherItem: WeatherItem;
+    constructor(private weatherService: DashboardWeatherService) {}
 
-  constructor(private weatherService: DashboardWeatherService) {}
-
-  ngOnInit() {
-    this.weatherService.getWeatherData().subscribe(
-        data => {
-            const weatherBinz = new WeatherItem(
-                data.name,
-                data.clouds,
-                data.weather[0].description,
-                data.main.temp,
-                data.wind.speed,
-                data.wind.deg,
-                data.rain
-            );
-        }
-    );
-  }
-
+    ngOnInit() {
+        this.weatherService.getWeatherData().subscribe(
+            data => {
+                this.weatherItem = new WeatherItem(
+                    data.name,
+                    data.weather[0].description,
+                    data.main.humidity,
+                    data.main.temp,
+                    data.wind.speed,
+                    data.wind.deg
+                );
+                console.log(this.weatherItem);
+            }
+        );
+    }
 }
--- akaflieg-ui/src/app/dashboard/dashboard-weather/weather-item.ts
+++ akaflieg-ui/src/app/dashboard/dashboard-weather/weather-item.ts
@@ -3,38 +3,9 @@
  */
 export class WeatherItem {
     constructor(public _cityName: string,
-                public _clouds: string,
                 public _cloudsDescription: string,
+                public _humidity: number,
                 public _temperature: number,
                 public _windSpeed: number,
-                public _windDirection: number,
-                public _rain: number) {}
-
-    private get cityName(): string {
-        return this._cityName;
-    }
-
-    private get clouds(): string {
-        return this._clouds;
-    }
-
-    private get cloudsDescription(): string {
-        return this._cloudsDescription;
-    }
-
-    private get temperature(): number {
-        return this._temperature;
-    }
-
-    private get windSpeed(): number {
-        return this._windSpeed;
-    }
-
-    private get windDirection(): number {
-        return this._windDirection;
-    }
-
-    private get rain(): number {
-        return this._rain;
-    }
+                public _windDirection: number) {}
 }
