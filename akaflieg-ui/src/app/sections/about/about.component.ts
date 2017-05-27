eb3b2e3943fdb9a5f40686fd78e994815161949b
shominees <jompa010@gmail.com>
2017-05-27 13:14:10 +0200

    added google maps exempt

--- akaflieg-ui/src/app/sections/about/about.component.ts
+++ akaflieg-ui/src/app/sections/about/about.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+declare const google: any;
 
 @Component({
   selector: 'app-about',
@@ -10,6 +11,25 @@ export class AboutComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    const gmapKey = 'AIzaSyDMb-EA7VWCNijtDppiizhkSBoMXf9Qfz8';
+    const label = 'Halle der Akaflieg Köln';
+    const zoom = 17;
+    const lat = 50.40357;
+    const lng = 6.528953;
+    this.mapInit(lat, lng, zoom, label);
   }
 
+  mapInit(lat, lng, zoom, label) {
+    const coords = {lat: lat, lng: lng};
+
+    const map = new google.maps.Map(document.getElementById('map'), {
+      zoom: zoom,
+      center: coords
+    });
+    const marker = new google.maps.Marker({
+      label: label,
+      position: coords,
+      map: map
+    });
+  }
 }
