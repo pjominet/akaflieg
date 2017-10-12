99a40b21834fa7afec562a128845236301333bfe
Tom Schlink <schlink.tom@hotmail.com>
2017-10-13 00:16:09 +0200

    activate h2 database and add rest controller for test purposes

---
+++ akaflieg-server/src/main/java/controller/LoginController.java
@@ -0,0 +1,13 @@
+package controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class LoginController {
+
+    @RequestMapping("/auth")
+    public String login(){
+        return "login";
+    }
+}
