9a0a7b7f9980c2ac9c4ffd6e657f4ddcd36b3143
Tom Schlink <tschlink@pictet-technologies.com>
2017-10-13 09:55:50 +0200

    change package structure to allow spring boot to find controllers

--- akaflieg-server/src/main/java/tech/clusterfunk/akaflieg/AkafliegApplication.java
+++ akaflieg-server/src/main/java/tech/clusterfunk/akaflieg/AkafliegApplication.java
@@ -1,4 +1,4 @@
-package akaflieg;
+package tech.clusterfunk.akaflieg;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- akaflieg-server/src/main/java/tech/clusterfunk/akaflieg/entities/User.java
+++ akaflieg-server/src/main/java/tech/clusterfunk/akaflieg/entities/User.java
@@ -1,4 +1,4 @@
-package entities;
+package tech.clusterfunk.akaflieg.entities;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
