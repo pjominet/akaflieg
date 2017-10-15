62f6465420b10b9c999ea2fa95d95e6c839c7a93
Tom Schlink <schlink.tom@hotmail.com>
2017-10-15 21:42:14 +0200

    add routine to create default user when database does not exist

--- akaflieg-server/src/main/java/tech/clusterfunk/akaflieg/entities/AkaUser.java
+++ akaflieg-server/src/main/java/tech/clusterfunk/akaflieg/entities/AkaUser.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.util.List;
 
 @Entity
-@Table(name = "akaUser")
+@Table(name = "akaUser", schema = "akaflieg")
 public class AkaUser {
 
     @Id
--- akaflieg-server/src/main/java/tech/clusterfunk/akaflieg/entities/NewsArticle.java
+++ akaflieg-server/src/main/java/tech/clusterfunk/akaflieg/entities/NewsArticle.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.time.LocalDate;
 
 @Entity
-@Table(name = "akaArticle")
+@Table(name = "akaArticle", schema = "akaflieg")
 public class NewsArticle {
 
     @Id
