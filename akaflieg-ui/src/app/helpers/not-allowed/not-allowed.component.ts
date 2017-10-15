999228478421c552ab9f2ea2f986b4724d7ce096
Patrick <jompa010@gmail.com>
2017-10-16 01:19:58 +0200

    refactorings to match project code style

--- akaflieg-ui/src/app/helpers/not-allowed/not-allowed.component.ts
+++ akaflieg-ui/src/app/helpers/not-allowed/not-allowed.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
-  selector: 'app-not-allowed',
-  templateUrl: './not-allowed.component.html',
-  styleUrls: ['./not-allowed.component.css']
+    selector: 'app-not-allowed',
+    templateUrl: './not-allowed.component.html',
+    styleUrls: ['./not-allowed.component.scss']
 })
 export class NotAllowedComponent implements OnInit {
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
 }
