02e52fe9023693040eb61b0ebdb8929f7dd385ae
Patrick <jompa010@gmail.com>
2017-10-16 01:06:16 +0200

    added find all method

--- akaflieg-server/src/main/java/tech/clusterfunk/akaflieg/repository/UserRepository.java
+++ akaflieg-server/src/main/java/tech/clusterfunk/akaflieg/repository/UserRepository.java
@@ -3,10 +3,14 @@ package tech.clusterfunk.akaflieg.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import tech.clusterfunk.akaflieg.entities.AkaUser;
 
+import java.util.List;
+
 public interface UserRepository extends JpaRepository<AkaUser, Long> {
 
     AkaUser findByUsername(String username);
 
     AkaUser findById(long id);
+
+    List<AkaUser> findAll();
 
 }
