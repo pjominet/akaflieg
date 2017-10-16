5c4861210e3933ee75c7990497313dda8ca9b191
shominees <jompa010@gmail.com>
2017-10-16 16:21:32 +0200

    reworked login component

--- akaflieg-ui/src/app/dashboard/login/login.component.ts
+++ akaflieg-ui/src/app/dashboard/login/login.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {LoginService} from './login.service';
-import {ActivatedRoute, Router} from '@angular/router';
+import {Router} from '@angular/router';
 import {AlertService} from '../../helpers/alert/alert.service';
 
 @Component({
@@ -11,34 +11,28 @@ import {AlertService} from '../../helpers/alert/alert.service';
 export class LoginComponent implements OnInit {
     model: any = {};
     loading = false;
-    stayLoggedIn = false;
-    returnUrl: string;
 
-    constructor(private route: ActivatedRoute,
-                private router: Router,
+    constructor(private router: Router,
                 private authenticationService: LoginService,
                 private alertService: AlertService) {
     }
 
     ngOnInit() {
-        // login if stay logged in is checked
-        if (this.stayLoggedIn) {
-            this.login()
-        }
-
         // reset login status
         this.authenticationService.logout();
-
-        // get return url from route parameters or default to '/dashboard'
-        this.returnUrl = this.route.snapshot.queryParams['returnUrl'] || '/dashboard';
     }
 
     login() {
         this.loading = true;
         this.authenticationService.login(this.model.username, this.model.password)
             .subscribe(
-                data => {
-                    this.router.navigate([this.returnUrl]);
+                result => {
+                    if (result === true) {
+                        this.router.navigate(['/dashboard/cms']);
+                    } else {
+                        this.alertService.error('Benutzername oder Passwort falsch');
+                        this.loading = false;
+                    }
                 },
                 error => {
                     this.alertService.error(error);
--- akaflieg-ui/src/app/dashboard/login/login.service.ts
+++ akaflieg-ui/src/app/dashboard/login/login.service.ts
@@ -1,25 +1,32 @@
 import {Injectable} from '@angular/core';
-import {Http, Response} from '@angular/http';
+import {Headers, Http, Response} from '@angular/http';
+import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/operator/map'
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/observable/throw';
 
 @Injectable()
 export class LoginService {
+    private headers = new Headers({'Content-Type': 'application/json'});
+
     constructor(private http: Http) {
     }
 
-    login(username: string, password: string) {
-        return this.http.post('/login', JSON.stringify({username: username, password: password}))
+    login(username: string, password: string): Observable<boolean> {
+        return this.http.post('/login',
+            JSON.stringify({username: username, password: password}), {headers: this.headers})
             .map((response: Response) => {
-                // login successful if there's a jwt token in the response
-                const user = response.headers;
-                console.log(user);
-                if (user) {
-                    // store user details and jwt token in local storage to keep user logged in between page refreshes
-                    localStorage.setItem('currentUser', JSON.stringify(user));
+                // login successful if there's a authorize token in the response
+                const token = response.json() && response.json().token;
+                console.log(token);
+                if (token) {
+                    // store user details and authorize token in local storage to keep user logged in between page refreshes
+                    localStorage.setItem('currentUser', JSON.stringify({username: username, token: token}));
+                    return true;
+                } else {
+                    return false
                 }
-
-                return user;
-            });
+            }).catch((error: any) => Observable.throw(error.json().error || 'Server error'));
     }
 
     logout() {
