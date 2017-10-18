f5c6bb53d1d727a63783d64346718ff651526cfc
shominees <jompa010@gmail.com>
2017-10-18 13:09:38 +0200

    added custom easing

--- akaflieg-ui/src/app/app.component.ts
+++ akaflieg-ui/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit} from '@angular/core';
+import {PageScrollConfig} from 'ng2-page-scroll';
 
 @Component({
     selector: 'app-root',
@@ -8,8 +9,23 @@ import {Component, OnInit} from '@angular/core';
 export class AppComponent implements OnInit {
 
     constructor() {
-    }
+        PageScrollConfig.defaultScrollOffset = -50;
+        PageScrollConfig.defaultEasingLogic = {
+            ease: (t: number, b: number, c: number, d: number): number => {
+                if (t === 0) {
+                    return b;
+                }
+                if (t === d) {
+                    return b + c;
+                }
+                if ((t /= d / 2) < 1) {
+                    return c / 2 * Math.pow(2, 10 * (t - 1)) + b;
+                }
+                return c / 2 * (-Math.pow(2, -10 * --t) + 2) + b;
+            }
+        }
+    };
 
-    ngOnInit(): void {
+    ngOnInit() {
     }
 }
