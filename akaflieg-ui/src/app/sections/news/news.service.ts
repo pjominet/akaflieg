8de81dc90ad3bea46a75663b6492981761d4c10d
Patrick <jompa010@gmail.com>
2017-11-02 22:23:12 +0100

    fixed wrong name, url

--- akaflieg-ui/src/app/sections/news/news.service.ts
+++ akaflieg-ui/src/app/sections/news/news.service.ts
@@ -13,17 +13,17 @@ export class NewsService {
             .map((res: Response) => res.json());
     }
 
-    public addItem(title: string, body: string) {
+    public addItem(title: string, content: string) {
         const headers = new Headers({'Content-Type': 'application/json'});
         const options = new RequestOptions({headers: headers});
 
         return this.http.post( environment.dataServiceURI + '/news/add',
-            JSON.stringify({title: title, body: body}), options)
+            JSON.stringify({title: title, content: content}), options)
             .map((res: Response) => res.json());
     }
 
     public getMock() {
-        return this.http.get(environment.dataServiceURI + './assets/data/news-mock.json')
+        return this.http.get( './assets/data/news-mock.json')
             .map((res: Response) => res.json());
     }
 }
