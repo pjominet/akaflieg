230fda30b5c61c8b15f8264622bcb2b94e9dad2b
Patrick <jompa010@gmail.com>
2017-11-02 22:23:48 +0100

    data unification

--- akaflieg-ui/src/app/dashboard/dashboard-weather/weather-item.ts
+++ akaflieg-ui/src/app/dashboard/dashboard-weather/weather-item.ts
@@ -1,11 +1,22 @@
 export class WeatherItem {
-    constructor(public cityName: string,
-                public pressure: string,
-                public cloudsDescription: string,
-                public iconName: string,
-                public humidity: number,
-                public temperature: number,
-                public windSpeed: number,
-                public windDirection: string) {
+    public cityName: string;
+    public pressure: string;
+    public cloudsDescription: string;
+    public iconName: string;
+    public humidity: number;
+    public temperature: number;
+    public windSpeed: number;
+    public windDirection: string;
+
+    constructor(cityName: string, pressure: string, cloudsDescription: string, iconName: string,
+                humidity: number, temperature: number, windSpeed: number, windDirection: string) {
+        this.cityName = cityName;
+        this.pressure = pressure;
+        this.cloudsDescription = cloudsDescription;
+        this.iconName = iconName;
+        this.humidity = humidity;
+        this.temperature = temperature;
+        this.windSpeed = windSpeed;
+        this.windDirection = windDirection;
     }
 }
