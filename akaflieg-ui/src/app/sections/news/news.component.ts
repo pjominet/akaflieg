2572c14c4a5499011891fbdef80e61ff9ffd74a5
Patrick <jompa010@gmail.com>
2017-11-17 13:39:53 +0100

    removed unused imports

--- akaflieg-ui/src/app/sections/news/news.component.ts
+++ akaflieg-ui/src/app/sections/news/news.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit, AfterViewInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {NewsItem} from './news-item';
 import {NewsService} from './news.service';
 import {environment} from '../../../environments/environment';
