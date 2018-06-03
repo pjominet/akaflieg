ec74062bf7dd6f7c02fd14f570ad93124648a072
Patrick <jompa010@gmail.com>
2018-06-03 11:59:30 +0200

    experimented with alert modals

--- akaflieg-client/src/app/helpers/alert/alert.component.ts
+++ akaflieg-client/src/app/helpers/alert/alert.component.ts
@@ -1,13 +1,18 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {AlertService} from './alert.service';
+import {ElementRef, ViewChild} from '@angular/core';
+
+declare var $;
 
 @Component({
     selector: 'app-alert',
     templateUrl: './alert.component.html',
     styleUrls: ['./alert.component.scss']
 })
-export class AlertComponent implements OnInit {
+export class AlertComponent implements OnInit, AfterViewInit {
     message: any;
+
+    @ViewChild('alertModal') alertModal: ElementRef;
 
     constructor(private alertService: AlertService) {
     }
@@ -16,6 +21,11 @@ export class AlertComponent implements OnInit {
         this.alertService.getMessage().subscribe(message => {
             this.message = message;
         });
+    }
+
+    ngAfterViewInit() {
+        if (this.message)
+            $(this.alertModal.nativeElement).modal('show');
     }
 
 }
