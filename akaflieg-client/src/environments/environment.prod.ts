6a16f9b8219e7dcac07e89fcc8531cf98473c31a
Patrick <jompa010@gmail.com>
2018-11-22 17:45:57 +0100

    fixed prod url

--- akaflieg-client/src/environments/environment.prod.ts
+++ akaflieg-client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
     production: true,
-    dataServiceURI: 'https://localhost:8888',
+    dataServiceURI: 'https://akaflieg.koeln:8888',
     weatherServiceURI: 'https://api.openweathermap.org/data/2.5/weather',
     mailTo: 'info'
 };
